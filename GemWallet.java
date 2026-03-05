7e38e212317f308b05fd6a266fe4f4b209cd1a73
annya-s <annya.sriram53@gmail.com>
2026-03-05 16:32:25 +0800

    -

--- GemWallet.java
+++ GemWallet.java
@@ -1,4 +1,20 @@
 public class GemWallet {
-    
+    private Map<GemColour, int> gems;
+
+    public void add(GemColour colour, int thisInt) {
+        // add code - Looi Le
+    }
+
+    public boolean remove(GemColour colour, int thisInt) {
+        // add code - Looi Le
+    }
+
+    public int total() {
+        // add code - Looi Le
+    }
+
+    public int get(GemColour colour) {
+        // add code - Looi Le
+    }
 
 }
