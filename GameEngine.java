cd6da741c3e7ca35d087d660d839a153fbb979c1
Lee Looi Le <116087284+LooiLe@users.noreply.github.com>
2026-03-22 15:46:22 +0800

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -7,9 +7,9 @@ public class GameEngine {
         // int numOfPlayers = promptNumPlayers(sc);
         // List<Player> players = createPlayers(sc, numOfPlayers);
 
-        List<Card> levelOneDeck = new ArrayList<>();
+        List<Noble> levelOneDeck = new ArrayList<>();
         try {
-            levelOneDeck = loadCards("cards.csv", 2);
+            levelOneDeck = loadNobles("nobles.csv");
             System.out.println(levelOneDeck.toString());
         } catch (InvalidFileException e) {
             System.out.println(e.getMessage());
@@ -83,5 +83,30 @@ public class GameEngine {
             default:
                 return GemColor.SAPPHIRE;
         }
+    }
+
+    public static List<Noble> loadNobles(String filename) throws InvalidFileException {
+        try (Scanner sc = new Scanner(new File(filename))) {
+            sc.nextLine();
+            List<Noble> nobles = new ArrayList<>();
+            while (sc.hasNext()) {
+                String[] cur = sc.nextLine().split(",");
+                Map<GemColor, Integer> cost = new HashMap<>();
+                cost.put(GemColor.DIAMOND, Integer.parseInt(cur[1]));
+                cost.put(GemColor.ONYX, Integer.parseInt(cur[2]));
+                cost.put(GemColor.EMERALD, Integer.parseInt(cur[3]));
+                cost.put(GemColor.RUBY, Integer.parseInt(cur[4]));
+                cost.put(GemColor.SAPPHIRE, Integer.parseInt(cur[5]));
+
+                nobles.add(new Noble(cur[0], cost));
+            }
+            return nobles;
+        } catch (FileNotFoundException e) {
+            throw new InvalidFileException(String.format("File (%s) not found!!", filename));
+        }
+    } 
+
+    public static GemCollection buildGemBank(int numPlayers) {
+        s
     }
 }
