c3793e9b1fe5b78ba1cab0c0bd5821ec9ba3d468
murtsmu <murtazaa.k.2025@computing.smu.edu.sg>
2026-03-31 16:52:58 +0800

    Update Card.java

--- src/splendor/entity/card/Card.java
+++ src/splendor/entity/card/Card.java
@@ -2,22 +2,46 @@ package splendor.entity.card;
 
 import splendor.valueobjects.*;
 
+/**
+ * Abstract base class for all cards in the game.
+ */
 public abstract class Card {
     private int points;
     private Cost cost;
 
+    /**
+     * Constructs a card with the given points and cost.
+     *
+     * @param points the prestige points awarded by this card
+     * @param cost   the gem cost required to purchase this card
+     */
     public Card(int points, Cost cost) {
         this.points = points;
         this.cost = cost;
     }
 
+    /**
+     * Returns the prestige points of this card.
+     *
+     * @return the point value
+     */
     public int getPoints() {
         return points;
     }
 
+    /**
+     * Returns the cost of this card.
+     *
+     * @return the cost object
+     */
     public Cost getCost() {
         return cost;
     }
 
+    /**
+     * Returns a string representation of this card.
+     *
+     * @return a string describing the card
+     */
     public abstract String toString();
 }
